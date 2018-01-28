4775b6d3c11e5e61d24e3ab9194cd432690985c5
mrzhuzhe <466648706@qq.com>
2018-01-28 22:45:08 +0800

    remove link

--- routes/middleware.js
+++ routes/middleware.js
@@ -23,7 +23,7 @@ exports.initLocals = function (req, res, next) {
 		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
-		{ label: 'Punisher', key: 'punisher', href: '/punisher' },
+		//	{ label: 'Punisher', key: 'punisher', href: '/punisher' },
 	];
 	res.locals.user = req.user;
 	next();
