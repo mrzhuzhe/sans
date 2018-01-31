07f6333211440c9be0c688107737926bcc4ee7b6
mrzhuzhe <466648706@qq.com>
2018-02-01 02:57:24 +0800

    add sign up

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,8 @@ exports = module.exports = function (app) {
 	app.all('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.get('/punisher', routes.views.punisher);
-	app.all('/contact', routes.views.contact);
+	app.all('/signup', routes.views.signup);
+	app.get('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
---
+++ routes/views/signup.js
@@ -0,0 +1,65 @@
+// @file signup.js
+// @path /routes/views/signup.js
+// @description Handles the post request when the user tries to sign up.
+// @url https://github.com/keystonejs/generator-keystone/issues/10
+//
+var keystone = require('keystone');
+var User = keystone.list('User');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'signup';
+	locals.filters = {
+	};
+	locals.data = {
+	};
+
+	console.log(req);
+
+	locals.formData = req.body || {};
+
+	view.on('post', { action: 'user.create' }, function(next) {
+
+		// if (!keystone.security.csrf.validate(req)) {
+		// 	req.flash('error', 'There was an error with your request, please try again.');
+		// 	return renderView();
+		// }
+
+		if(locals.formData.password !== locals.formData.password_confirm){
+      req.flash('error', 'The passwords do not match.');
+			next();
+		}
+
+		var newUser = new User.model({
+			name: {
+				first: locals.formData.first,
+				last: locals.formData.last
+			},
+			email: locals.formData.email,
+			password: locals.formData.password
+      //Add some user defaults here.
+		});
+
+		newUser.isAdmin = false;
+
+		newUser.save(function(err, result) {
+			if (err) {
+				locals.data.validationErrors = err.errors;
+			} else {
+				req.flash('success', 'Account created. Please sign in.');
+
+				//auto-signin can be easily implemented using the keystone.session.signin() API.
+				return res.redirect('/keystone/signin');
+			}
+			next();
+		});
+
+	});
+
+	// Render the view
+	view.render('signup');
+};
