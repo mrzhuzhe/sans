05e6ad218b78d0ea974248f8577fce3eee47d386
mrzhuzhe <466648706@qq.com>
2026-03-07 23:13:35 +0800

    abi

--- pages/post/llvm-backend.js
+++ pages/post/llvm-backend.js
@@ -47,9 +47,10 @@ function post({ data }) {
                       <h3>2. 相关工作</h3>
                       <ul>
                         <li>1. 自定义指令DSP，浮点数，多线程，图优化</li>
-                        <li>2. LLVM backend 的结构和功能划分, 指令选择，指令合并，指令legalize</li>
+                        <li>2. LLVM backend 的结构和功能划分，指令选择，指令合并，指令legalize</li>
                         <li>3. LLVM IR / Machine IR / tableGen </li>
                         <li>4. 如何更高效的组织汇编代码，如何规划栈和堆，函数调用</li>
+                        <li>5. 本次大部分代码都来自 LLVM官方教程中backend部分文档例子CPU0 </li>
                       </ul>
 
                       <h3>3. 实验设计</h3>
@@ -62,24 +63,27 @@ function post({ data }) {
 
                       <h3>4. 实验结果</h3>
                       <ul>
-                        <li>1. 通过增加二元指令debug llvm 全流程</li>
-                        <li>2. 调试LLVM的小技巧</li>
+                        <li>1. 通过增加二元指令debug llvm 全流程 见PR <a target="_blank" href='https://github.com/mrzhuzhe/LLVM-12X/commit/624c280c8d169642bf42cdcc6f54f0f1eca822c7'>https://github.com/mrzhuzhe/LLVM-12X/commit/624c280c8d169642bf42cdcc6f54f0f1eca822c7</a></li>
                       </ul>
 
 
                       <h3>5. 总结和展望</h3>
                       <ul>
                         <li>1. 编译器本身就是一个虚拟机</li>
-                        <li>2. LLVM 的目前的路线是什么？还没解决的问题有哪些？
-                            <br></br>（1）过度设计？
-                        </li>
+                        <li>2. frame , ABI, libc操作系统编程接口</li>
+                        <li>3. LLVM 的目前的路线是什么？还没解决的问题有哪些？</li>
+                        <li>4. 过度设计？</li>
+                        <li>5. testSuite and benchmark</li>
+                        <li>6. LLVM codgen流程为 LLParse &gt; prelogue &gt;  type leglize &gt; selectDag &gt; combineDag &gt; register allocation &gt; epilogue</li>
+                        <li>7. 多核？</li>
                       </ul>
 
                       <h3>6. 参考资料</h3>
                       <ul>
                         <li>1. LLVM官方教程CPU0 <a href="https://jonathan2251.github.io/lbd/backendstructure.html" target="_blank" >https://jonathan2251.github.io/lbd/backendstructure.html</a></li>
                         <li>2. learn llvm 12 <a href="https://github.com/PacktPublishing/Learn-LLVM-12" target="_blank" >https://github.com/PacktPublishing/Learn-LLVM-12</a></li>                        
-                        <li>3. 《深入理解LLVM：代码生成》</li>
+                        <li>3. 《深入理解LLVM：代码生成》彭成寒</li>
+                        <li>4. 龙书 </li>
                       </ul>
                     </div>
                     
